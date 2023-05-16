5a497984d0d26e233806142b185e2b78702b91ea
Kitti <kittitoth8@gmail.com>
2023-05-16 13:34:34 +0200

    move index.js file to api folder

--- api/index.js
+++ api/index.js
@@ -2,9 +2,9 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
+app.get("/api", (req, res) => {
   res.send("teszt");
-})
+});
 
 
 app.listen(port, ()=> {
